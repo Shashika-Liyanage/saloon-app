6746ae35e7da2c77c1e8ada6944b694f16358f5a
mihiranga-official <mihiranga.office@gmail.com>
2024-04-01 23:58:54 +0530

    Booking Page UI

--- src/pages/Booking/BookingPage.js
+++ src/pages/Booking/BookingPage.js
@@ -9,7 +9,6 @@ import {
   Typography,
 } from "@mui/material";
 import Autocomplete from "@mui/material/Autocomplete";
-import { DatePicker } from "@mui/x-date-pickers";
 
 const BookingPage = () => {
   const [age, setAge] = useState("");
@@ -17,155 +16,164 @@ const BookingPage = () => {
   const handleChange = (event, value) => {
     setAge(value);
   };
-  const [startDate, setStartDate] = useState(new Date());
-  return (
-    <>
-      <Box height={700} width={900} my={8}>
-        <form>
-          <CardContent sx={{ alignContent: "center", textAlign: "center" }}>
-            <Typography sx={{ fontWeight: 700, fontSize: 40, mt: "10px" }}>
-              Book An Appointment
-            </Typography>
 
-            <Grid
-              container
-              spacing={3}
-              justifyContent={"center"}
-              sx={{ mt: "5px" }}
+  return (
+    
+    <Box sx={{ display: 'flex', justifyContent: 'space-between'}}>
+    <Typography  sx={{ml:26, textTransform: 'uppercase',  textAlign: 'center', fontWeight: 700, fontSize: 50, mt: "100px", color: "#824D74" }}>
+     we style,<br />  while<br/> you<br/> smile
+    </Typography>
+      <form>
+        <CardContent
+          sx={{
+            alignContent: "center",
+            textAlign: "center",
+            color: "#513162",
+            boxShadow: "0 20px 0px rgba(0,0,0,0.1)",
+            borderRadius: 8,
+            padding: 4,
+            "&:hover": {
+              backgroundColor: "#dddddd", // Change background color on hover
+              color: "#F8F6E3", // Change text color on hover
+            },
+          }}
+        >
+          <Typography sx={{ fontWeight: 700, fontSize: 40, mt: "10px" ,color:"#FC6736"}}>
+            Book An Appointment
+          </Typography>
+
+          <Grid
+            container
+            spacing={3}
+            justifyContent={"center"}
+            sx={{ mt: "5px" }}
+          >
+            <Grid item xs={6}>
+              <TextField
+                id="outlined-basic"
+                required
+                label="Name"
+                variant="outlined"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                id="outlined-basic"
+                required
+                label="Enter Phone Number"
+                variant="outlined"
+                fullWidth
+              />
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            spacing={3}
+            justifyContent={"center"}
+            sx={{ mt: "5px" }}
+          >
+            <Grid item xs={6}> {/* Adjusted grid width */}
+              <TextField
+                id="outlined-basic"
+                required
+                label="Email"
+                variant="outlined"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={6}> {/* Adjusted grid width */}
+              <Autocomplete
+                id="demo-simple-select-helper"
+                options={[
+                  { value: 10, label: "Gampaha" },
+                  { value: 20, label: "Veyangoda" },
+                  { value: 30, label: "Naiwala" },
+                  { value: 30, label: "Minuwangoda" },
+                  { value: 30, label: "Colombo" },
+                ]}
+                getOptionLabel={(option) => option.label}
+                value={age}
+                onChange={handleChange}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Branch"
+                    variant="outlined"
+                    required
+                  />
+                )}
+              />
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            spacing={3}
+            justifyContent={"center"}
+            sx={{ mt: "5px" }}
+          >
+            <Grid item xs={6}>
+              <Autocomplete
+                id="demo-simple-select-helper"
+                options={[
+                  { value: 10, label: "Gampaha" },
+                  { value: 20, label: "Veyangoda" },
+                  { value: 30, label: "Naiwala" },
+                  { value: 30, label: "Minuwangoda" },
+                  { value: 30, label: "Colombo" },
+                ]}
+                getOptionLabel={(option) => option.label}
+                value={age}
+                onChange={handleChange}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Service"
+                    variant="outlined"
+                    required
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item xs={6}></Grid>
+          </Grid>
+          <Grid
+            container
+            spacing={3}
+            justifyContent={"center"}
+            sx={{ mt: "5px" }}
+          >
+            <Grid item xs={6}>
+              <TextField
+                id="outlined-basic"
+                required
+                label="Name"
+                variant="outlined"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                id="outlined-basic"
+                required
+                label="Name"
+                variant="outlined"
+                fullWidth
+              />
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Button
+              sx={{ mt: "25px", width: "400px" }}
+              size="large"
+              variant="contained"
             >
-              <Grid item xs={3}>
-                <TextField
-                  id="outlined-basic"
-                  required
-                  label="Name"
-                  variant="outlined"
-                  fullWidth
-                />
-              </Grid>
-              <Grid item xs={3}>
-                <TextField
-                  id="outlined-basic"
-                  required
-                  label="Enter Phone Number"
-                  variant="outlined"
-                  fullWidth
-                />
-              </Grid>
-            </Grid>
-            <Grid
-              container
-              spacing={3}
-              justifyContent={"center"}
-              sx={{ mt: "5px" }}
-            >
-              <Grid item xs={3}>
-                <TextField
-                  id="outlined-basic"
-                  required
-                  label="Email"
-                  variant="outlined"
-                />
-              </Grid>
-              <Grid item xs={3}>
-                <Autocomplete
-                  id="demo-simple-select-helper"
-                  options={[
-                    { value: 10, label: "Gampaha" },
-                    { value: 20, label: "Veyangoda" },
-                    { value: 30, label: "Naiwala" },
-                    { value: 30, label: "Minuwangoda" },
-                    { value: 30, label: "Colombo" },
-                  ]}
-                  getOptionLabel={(option) => option.label}
-                  value={age}
-                  onChange={handleChange}
-                  renderInput={(params) => (
-                    <TextField
-                      {...params}
-                      label="Branch"
-                      variant="outlined"
-                      required
-                    />
-                  )}
-                />
-              </Grid>
-            </Grid>
-            <Grid
-              container
-              spacing={3}
-              justifyContent={"center"}
-              sx={{ mt: "5px" }}
-            >
-              <Grid item xs={3}>
-                <Autocomplete
-                  id="demo-simple-select-helper"
-                  options={[
-                    { value: 10, label: "Gampaha" },
-                    { value: 20, label: "Veyangoda" },
-                    { value: 30, label: "Naiwala" },
-                    { value: 30, label: "Minuwangoda" },
-                    { value: 30, label: "Colombo" },
-                  ]}
-                  getOptionLabel={(option) => option.label}
-                  value={age}
-                  onChange={handleChange}
-                  renderInput={(params) => (
-                    <TextField
-                      {...params}
-                      label="Service"
-                      variant="outlined"
-                      required
-                    />
-                  )}
-                />
-              </Grid>
-              <Grid item xs={3}>
-                <DatePicker
-                  selected={date}
-                  onChange={handleDateChange}
-                  showTimeSelect
-                  dateFormat="Pp"
-                />
-              </Grid>
-            </Grid>
-            <Grid
-              container
-              spacing={3}
-              justifyContent={"center"}
-              sx={{ mt: "5px" }}
-            >
-              <Grid item xs={3}>
-                <TextField
-                  id="outlined-basic"
-                  required
-                  label="Name"
-                  variant="outlined"
-                  fullWidth
-                />
-              </Grid>
-              <Grid item xs={3}>
-                <TextField
-                  id="outlined-basic"
-                  required
-                  label="Name"
-                  variant="outlined"
-                  fullWidth
-                />
-              </Grid>
-            </Grid>
-            <Grid item>
-              <Button
-                sx={{ mt: "25px", width: "430px" }}
-                size="large"
-                variant="contained"
-              >
-                Book
-              </Button>
-            </Grid>
-          </CardContent>
-        </form>
-      </Box>
-    </>
+              Book
+            </Button>
+          </Grid>
+        </CardContent>
+      </form>
+    </Box>
   );
 };
 
