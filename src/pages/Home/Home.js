60c214a153849248ea3f39677df8a17aec541e54
mihiranga-official <mihiranga.office@gmail.com>
2024-04-17 12:34:21 +0530

    some mode

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,40 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../../services/firebaseConfig";
 import toast, { Toaster } from "react-hot-toast";
+import { Avatar, Button, Grid } from "@mui/material";
 
 const Home = () => {
   const navigate = useNavigate();
-  
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userData) => {
+      setUser(userData);
+    });
+
+    return () => {
+      unsubscribe(); // Unsubscribe from the auth state listener when component unmounts
+    };
+  }, []);
+
   const handleLogout = () => {
-    
     signOut(auth)
       .then(() => {
-        
         // Sign-out successful.
         navigate("/login");
-        console.log("Signed out successfully");
+
         // Show toast notification
-        toast.success('Logged out successfully.');
+        toast.success("Logged out successfully.");
       })
       .catch((error) => {
         // An error happened.
-        toast.error("Error logging out"); // Display error toast if sign-out fails
+
+        toast.error("Error logging out. Please try again.");
       });
   };
-  
+
+  // Function to extract first part of the email address before the "@"
+  const getFirstNameFromEmail = (email) => {
+    return email.split("@")[0];
+  };
+
   return (
     <>
-     
-        <p>Welcome Home</p>
+      <Grid container alignItems="center" justifyContent="center">
+        <Avatar
+          alt="Remy Sharp"
+          src="/static/images/avatar/1.jpg"
+          sx={{ width: 156, height: 156 }}
+        />
+      </Grid>
 
-        <div>
-          <button onClick={handleLogout}>Logout</button>
-          <Toaster position="top-right"/>
+      <Grid container alignItems="center" justifyContent="center">
+      <Grid alignText="center">
+        <h1 style={{color:"#824D74"}}>Hello </h1>
+        <div style={{ display: "block",color:"#824D74" ,fontWeight:"bold"}}>
+          {user && <h1>{getFirstNameFromEmail(user.email)}</h1>}
         </div>
-    
-     
+        <div>
+          <Button variant="contained" onClick={handleLogout}>
+            Log out
+          </Button>
+          <Toaster position="top-right" />
+        </div>
+      </Grid>
+      </Grid>
     </>
   );
 };
