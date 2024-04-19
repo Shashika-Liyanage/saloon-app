97787a4eaafbacda957968f4c38ee367b32eebbf
mihiranga-official <mihiranga.office@gmail.com>
2024-04-19 19:46:51 +0530

    some mode done

--- src/pages/Layout/Layout.js
+++ src/pages/Layout/Layout.js
@@ -1,37 +1,80 @@
 import { Outlet, Link } from "react-router-dom";
 import React from "react";
+import "./Layout.css"; //
+import { Button, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
 const Layout = () => {
+  const navigate = useNavigate();
+
+  const login = () => {
+    navigate("/login");
+  };
+  const headerStyle = {
+    height: "80px",
+    color: "#fff",
+    display: "flex",
+    alignItems: "center",
+    padding: "0 20px",
+  };
+
+  const salonNameStyle = {
+    marginRight: "auto",
+    fontFamily: "Arial, sans-serif",
+    fontWeight: "bold",
+    fontSize: "15px",
+    letterSpacing: "2px",
+    textTransform: "uppercase",
+  };
+
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/home">Home</Link>
-          </li>
-          <li>
-            <Link to="/booking">BookingPage</Link>
-          </li>
-          <li>
-            <Link to="/signup">Sign Up</Link>
-          </li>
-          <li>
-            <Link to="/tableshowing">Temp Table</Link>
-          </li>
-          <li>
-            <Link to="/tableshowing2">Temp Table2</Link>
-          </li>
-        </ul>
-      </nav>
+      <div style={headerStyle}>
+        <Typography
+          variant="h6"
+          sx={{ mt: "5px" }}
+          component="h6"
+          style={salonNameStyle}
+        >
+          <nav className="horizontal-nav">
+            <ul>
+              <li>
+                <Link to="/dashboard">Dashboard</Link>
+              </li>
+              <li>
+                <Link to="/home">Home</Link>
+              </li>
+              <li>
+                <Link to="/booking">BookingPage</Link>
+              </li>
+              <li>
+                <Link to="/tableshowing">Temp Table</Link>
+              </li>
+              <li>
+                <Link to="/tableshowing2">Temp Table2</Link>
+              </li>
+              <li>
+                <Button
+                  variant="outlined"
+                  sx={{
+                    borderRadius: "20px",
+                    fontWeight: "700",
+                    "&:hover": { backgroundColor: "#D20062", color: "#F8F6E3" },
+                    color: "black",
+                  }}
+                  onClick={login}
+                >
+                  Login
+                </Button>
+              </li>
+            </ul>
+          </nav>
 
-      <Outlet />
+          <Outlet />
+        </Typography>
+      </div>
     </>
-  )
+  );
 };
 
 export default Layout;
