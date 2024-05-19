3f6e99e051dfa03e0e7975312a7e35d8f5f06f48
Shashika-Liyanage <nayanamini2001@gmail.com>
2024-05-19 21:10:28 +0530

    UI updated

--- src/pages/admin/Update/PriceUpdate.js
+++ src/pages/admin/Update/PriceUpdate.js
@@ -27,7 +27,9 @@ const PriceUpdate = () => {
   const { firebaseId } = useParams();
   const [typeOptions, setTypeOptions] = useState([]);
   const [priceOptions, setPriceOptions] = useState([]);
+  
   useEffect(() => {
+  //fetch data use for hair section
     const fetchData = async () => {
       const db = getDatabase();
       const dbRef = ref(db, "createprice/haircut/"+firebaseId);
@@ -66,6 +68,7 @@ const PriceUpdate = () => {
     fetchData();
   }, [firebaseId]);
 
+//overwrite data (update) use for hair section
   const overWriteData = async () => {
     const db = getDatabase(app);
     const newPostRef = ref(db, "createprice/haircut/"+firebaseId);
@@ -80,6 +83,8 @@ const PriceUpdate = () => {
         toast.error("Data has not created ");
       });
   };
+
+//delete data for hair section
 const deleteData =async(saloonIdParam)=>{
   const db=getDatabase(app)
   const dbRef=ref(db,"createprice/haircut/"+saloonIdParam);
@@ -124,7 +129,7 @@ const deleteData =async(saloonIdParam)=>{
   const handlebridalButtonClick = () => {
     setshowDressingModal(true);
   };
-  // Function to close the modal  in Boyd section
+  // Function to close the modal  in Body section
   const handleCloseModalBridal = () => {
     setshowDressingModal(false);
   };
@@ -133,7 +138,7 @@ const deleteData =async(saloonIdParam)=>{
     setShowWaxingModal(true);
     setshowDetainingModal(true);
   };
-  // Function to close the modal  in Boyd section
+  // Function to close the modal  in Body section
   const handleCloseModalBody = () => {
     setShowWaxingModal(false);
     setshowDetainingModal(false);
@@ -207,21 +212,7 @@ const deleteData =async(saloonIdParam)=>{
                   Update Hair Cut Price{" "}
                 </Button>
               </Grid>
-              <Grid item xs={4}>
-                <Button variant="contained" onClick={setshowHairColoringModal}>
-                  Update Hair Coloring Price{" "}
-                </Button>
-              </Grid>{" "}
-              <Grid item xs={4}>
-                <Button variant="contained" onClick={setshowHairSpaModal}>
-                  Update Hair Spa Price{" "}
-                </Button>
-              </Grid>
-              <Grid item xs={4}>
-                <Button variant="contained" onClick={setshowHairTreatModal}>
-                  Update Hair Treatments Price{" "}
-                </Button>
-              </Grid>
+              
             </Stack>
           </CustomCard>
 
@@ -235,21 +226,7 @@ const deleteData =async(saloonIdParam)=>{
                   Update CleanUp Price{" "}
                 </Button>
               </Grid>
-              <Grid item xs={4}>
-                <Button onClick={setFacialModal} variant="contained">
-                  Update Facial Price{" "}
-                </Button>
-              </Grid>{" "}
-              <Grid item xs={4}>
-                <Button onClick={setThreadingModal} variant="contained">
-                  Update Threading Price{" "}
-                </Button>
-              </Grid>
-              <Grid item xs={4}>
-                <Button disabled variant="contained">
-                  Under constractions
-                </Button>
-              </Grid>
+             
             </Stack>
           </CustomCard>
           <CustomCard>
@@ -258,25 +235,11 @@ const deleteData =async(saloonIdParam)=>{
             </Typography>
             <Stack direction="row" spacing={2}>
               <Grid item xs={4}>
-                <Button onClick={setshowManicureModal} variant="contained">
-                  Update Manicure Price{" "}
-                </Button>
-              </Grid>
-              <Grid item xs={4}>
-                <Button onClick={setshowPedicureModal} variant="contained">
+              <Button onClick={setshowPedicureModal} variant="contained">
                   Update Pedicure Price{" "}
                 </Button>
-              </Grid>{" "}
-              <Grid item xs={4}>
-                <Button onClick={setshowMicroBladingModal} variant="contained">
-                  Update Microblading Price{" "}
-                </Button>
               </Grid>
-              <Grid item xs={4}>
-                <Button onClick={setshowNailNaModal} variant="contained">
-                  Update Nail Price{" "}
-                </Button>
-              </Grid>
+              
             </Stack>
           </CustomCard>
           <CustomCard>
@@ -289,21 +252,7 @@ const deleteData =async(saloonIdParam)=>{
                   Update Waxing Price{" "}
                 </Button>
               </Grid>
-              <Grid item xs={4}>
-                <Button onClick={handleBodyButtonClick} variant="contained">
-                  De-Tanning premium Price{" "}
-                </Button>
-              </Grid>{" "}
-              <Grid item xs={4}>
-                <Button disabled variant="contained">
-                  Update Hair Spa Price{" "}
-                </Button>
-              </Grid>
-              <Grid item xs={4}>
-                <Button disabled variant="contained">
-                  Update Hair Treatments Price{" "}
-                </Button>
-              </Grid>
+            
             </Stack>
           </CustomCard>
           <CustomCard>
@@ -316,21 +265,7 @@ const deleteData =async(saloonIdParam)=>{
                   Update DRESSING Price{" "}
                 </Button>
               </Grid>
-              <Grid item xs={4}>
-                <Button disabled variant="contained">
-                  De-Tanning premium Price{" "}
-                </Button>
-              </Grid>{" "}
-              <Grid item xs={4}>
-                <Button disabled variant="contained">
-                  Update Hair Spa Price{" "}
-                </Button>
-              </Grid>
-              <Grid item xs={4}>
-                <Button disabled variant="contained">
-                  Update Hair Treatments Price{" "}
-                </Button>
-              </Grid>
+             
             </Stack>
           </CustomCard>
         </Grid>
